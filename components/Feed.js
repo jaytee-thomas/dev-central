880d234dbd0b30a5bf2aba79457715bdd999c3bb
jaytee-thomas <jt.aws.cloud@protonmail.com>
2022-05-10 14:53:28 -0500

    added two more components

---
+++ components/Feed.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Feed() {
+  return <h1>Hello, I am the Feed</h1>
+}
+
+export default Feed
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
+import Feed from '../components/Feed'
+import Modal from '../components/Modal'
 
 const Home: NextPage = () => {
   return (
@@ -9,7 +11,14 @@ const Home: NextPage = () => {
         <title>devcentral.tech</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      {/*Header*/}
       <Header />
+
+      {/*Feed*/}
+      <Feed />
+
+      {/*Modal*/}
+      <Modal />
     </div>
   )
 }
